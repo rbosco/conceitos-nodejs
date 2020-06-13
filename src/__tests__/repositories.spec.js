49c4d3c2f41ca68199aabd90cc9a8997d8d151a1
compartilhaclub_ti <compartilhaclub.ti@gmail.com>
2020-06-13 12:01:13 -0300

    Repository CRUD

--- src/__tests__/repositories.spec.js
+++ src/__tests__/repositories.spec.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const app = require("../app");
-const { isUuid } = require("uuidv4");
+const { isUuid, uuid } = require("uuidv4");
 
 describe("Repositories", () => {
   it("should be able to create a new repository", async () => {
